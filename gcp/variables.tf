e0179c33ce438d9103032edd03f1273e6d483b63
DC <dc@technoexile.com>
2021-06-23 07:53:18 -0600

    bumped agoric sdk to agorictest-13 and nodejs to v14 for phase4 testnet

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -63,7 +63,7 @@ variable agoric_node_release {
 
   default = {
     repository = "https://github.com/Agoric/agoric-sdk"
-    tag        = "agorictest-12"
+    tag        = "agorictest-13"
   }
 }
 
