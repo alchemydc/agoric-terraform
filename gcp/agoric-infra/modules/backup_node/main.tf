e50d17f877f19623631a0713d7a97c88512566b8
DC <dc@technoexile.com>
2021-12-13 13:14:25 -0700

    bump validator to 1024G SSD, and backup node to 2T disk

--- gcp/agoric-infra/modules/backup_node/main.tf
+++ gcp/agoric-infra/modules/backup_node/main.tf
@@ -96,6 +96,6 @@ resource "google_compute_disk" "backup_node" {
   type = "pd-ssd"
   #type = "pd-standard"      #disk I/O doesn't yet warrant SSD backed validators
   # in GB
-  size                      = 250
+  size                      = 2048
   physical_block_size_bytes = 4096
 }
--- gcp/agoric-infra/modules/validator/main.tf
+++ gcp/agoric-infra/modules/validator/main.tf
@@ -96,6 +96,6 @@ resource "google_compute_disk" "validator" {
   type = "pd-ssd"
   #type = "pd-standard"      #slower but cheaper
   # in GB
-  size                      = 100
+  size                      = 1024
   physical_block_size_bytes = 4096
 }
